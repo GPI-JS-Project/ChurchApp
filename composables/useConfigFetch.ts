ee1c1977bec3bb61a6364214747e2af61350de50
Noval Smith <novalsmith69@gmail.com>
2024-03-09 21:28:11 +0700

    Connect the integration into service messaging

--- composables/useConfigFetch.ts
+++ composables/useConfigFetch.ts
@@ -1,5 +1,19 @@
 export const useConfigFetch: typeof useFetch = (request, opts?) => {
-  const config = useRuntimeConfig()
+const config = useRuntimeConfig();
 
   return useFetch(request, { baseURL: config.public.baseURL, ...opts })
+}
+
+export const useConfigPost = (request: string, data: any, opts?: any) => {
+  const config = useRuntimeConfig();
+  return useFetch(request, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+      ...opts?.headers,
+    },
+    body: JSON.stringify(data),
+    baseURL: config.public.baseURL,
+    ...opts,
+  });
 }
