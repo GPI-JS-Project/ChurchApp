d626c5c32ed78b10bf21670a1a35b6bcd23a6eae
Noval Smith <novalsmith69@gmail.com>
2024-12-10 10:48:36 +0700

    update

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,20 @@
 export default defineNuxtConfig({
   generate: {
     routes: ['/', '/birthday', '/event'], // List of routes to prerender
-    exclude: ['/dynamic-route'] 
+   },
+  app: {
+    head: {
+      title: "HIKANOS - gpijsjaktim",
+      link: [
+        { rel: 'icon', type: 'image/png', href: '/icons/icon512_rounded.png' },
+      ],
+      meta: [
+        { charset: "utf-8" },
+        { name: "viewport", content: 'width=device-width, initial-scale=1' },
+        { hid: "description", name: "description", content: "gpijsjaktim" }
+      ]
+    }
   },
- 
 
   modules: [
     // '@vite-pwa/nuxt', // Modul PWA
@@ -12,7 +23,7 @@ export default defineNuxtConfig({
     'dayjs-nuxt',
     // '@pinia/nuxt',
   ],
- 
+
   plugins: [
     './plugins/device.js'
   ],
@@ -48,5 +59,37 @@ export default defineNuxtConfig({
       }
     }
   },
- 
+
+  // Konfigurasi PWA
+ pwa: {
+    registerType: 'autoUpdate', // Mengatur tipe pendaftaran
+    manifest: {
+      name: 'GPI JS Jamtim',
+      short_name: 'GPIJSJAKTIMM',
+      description: 'GPi Jalan Suci Jaktim',
+      theme_color: '#ffffff',
+      icons: [
+        {
+          src: '/icons/icon512_rounded.png',
+          sizes: '512x512',
+          type: 'image/png'
+        }
+      ],
+    },
+    workbox: {
+      runtimeCaching: [
+        {
+          urlPattern: ({ request }) => request.destination === 'image',
+          handler: 'CacheFirst',
+          options: {
+            cacheName: 'images-cache',
+            expiration: {
+              maxEntries: 10,
+              maxAgeSeconds: 60 * 60 * 24 * 7, // 1 minggu
+            },
+          },
+        },
+      ],
+    },
+  },
 });
